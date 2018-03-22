42e336cb15248186a7d23fd0678b7c92e33ad670
Michaël Schuh <michael.schuh.34@gmail.com>
2018-03-23 00:11:13 +0100

    initial commit : minimax, alpha-beta, CLI

---
+++ cli.py
@@ -0,0 +1,155 @@
+import chess
+
+
+def inputMove():
+	move = input('Move:')
+
+
+def minimax(depth, board, alpha, beta, maximizePlayer):
+    if depth == 0:
+        return -evaluate(board)
+
+    bestScore = -10000 if maximizePlayer else 10000
+
+    for move in board.legal_moves:
+        board.push(move)
+        if maximizePlayer:
+            bestScore = max([ bestScore, minimax(depth - 1, board, alpha, beta, not maximizePlayer) ])
+            alpha = max([alpha, bestScore])
+        else:
+            bestScore = min([ bestScore, minimax(depth - 1, board, alpha, beta, not maximizePlayer) ])
+            beta = min([beta, bestScore])
+        board.pop()
+
+        if beta <= alpha:
+            return bestScore
+
+    return bestScore
+
+
+def findMove(depth):
+    bestScore, bestMove = -9999, None
+    for move in board.legal_moves:
+        board.push(move)
+        score = minimax(depth - 1, board, -10000, 10000, False)
+        board.pop()
+        if score > bestScore:
+            bestScore = score
+            bestMove = move
+
+    return bestMove
+
+
+def evaluate(board):
+
+    squareValues = {
+        chess.PAWN: [
+            [0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0],
+            [5.0,  5.0,  5.0,  5.0,  5.0,  5.0,  5.0,  5.0],
+            [1.0,  1.0,  2.0,  3.0,  3.0,  2.0,  1.0,  1.0],
+            [0.5,  0.5,  1.0,  2.5,  2.5,  1.0,  0.5,  0.5],
+            [0.0,  0.0,  0.0,  2.0,  2.0,  0.0,  0.0,  0.0],
+            [0.5, -0.5, -1.0,  0.0,  0.0, -1.0, -0.5,  0.5],
+            [0.5,  1.0, 1.0,  -2.0, -2.0,  1.0,  1.0,  0.5],
+            [0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0]
+        ],
+
+        chess.KNIGHT: [
+            [-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0],
+            [-4.0, -2.0,  0.0,  0.0,  0.0,  0.0, -2.0, -4.0],
+            [-3.0,  0.0,  1.0,  1.5,  1.5,  1.0,  0.0, -3.0],
+            [-3.0,  0.5,  1.5,  2.0,  2.0,  1.5,  0.5, -3.0],
+            [-3.0,  0.0,  1.5,  2.0,  2.0,  1.5,  0.0, -3.0],
+            [-3.0,  0.5,  1.0,  1.5,  1.5,  1.0,  0.5, -3.0],
+            [-4.0, -2.0,  0.0,  0.5,  0.5,  0.0, -2.0, -4.0],
+            [-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0]
+        ],
+
+        chess.BISHOP: [
+            [ -2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0],
+            [ -1.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -1.0],
+            [ -1.0,  0.0,  0.5,  1.0,  1.0,  0.5,  0.0, -1.0],
+            [ -1.0,  0.5,  0.5,  1.0,  1.0,  0.5,  0.5, -1.0],
+            [ -1.0,  0.0,  1.0,  1.0,  1.0,  1.0,  0.0, -1.0],
+            [ -1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0, -1.0],
+            [ -1.0,  0.5,  0.0,  0.0,  0.0,  0.0,  0.5, -1.0],
+            [ -2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0]
+        ],
+
+        chess.ROOK: [
+            [  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0],
+            [  0.5,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  0.5],
+            [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+            [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+            [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+            [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+            [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+            [  0.0,   0.0, 0.0,  0.5,  0.5,  0.0,  0.0,  0.0]
+        ],
+
+        chess.QUEEN: [
+            [ -2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0],
+            [ -1.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -1.0],
+            [ -1.0,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -1.0],
+            [ -0.5,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -0.5],
+            [  0.0,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -0.5],
+            [ -1.0,  0.5,  0.5,  0.5,  0.5,  0.5,  0.0, -1.0],
+            [ -1.0,  0.0,  0.5,  0.0,  0.0,  0.0,  0.0, -1.0],
+            [ -2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0]
+        ],
+
+        chess.KING: [
+            [ -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+            [ -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+            [ -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+            [ -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+            [ -2.0, -3.0, -3.0, -4.0, -4.0, -3.0, -3.0, -2.0],
+            [ -1.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -1.0],
+            [  2.0,  2.0,  0.0,  0.0,  0.0,  0.0,  2.0,  2.0 ],
+            [  2.0,  3.0,  1.0,  0.0,  0.0,  1.0,  3.0,  2.0 ]
+        ]
+    }
+
+    values = {
+        chess.PAWN: 10,
+        chess.KNIGHT: 30,
+        chess.BISHOP: 30,
+        chess.ROOK: 50,
+        chess.QUEEN: 90,
+        chess.KING: 1000
+    }
+
+    total = 0
+    for pieceType in values:
+        for color in [chess.WHITE, chess.BLACK]:
+            for piece in board.pieces(pieceType, color):
+                total += (values[pieceType] + squareValues[pieceType] \
+                    [::-1 if color is chess.WHITE else 1][int(piece / 8)][piece % 8]) \
+                    * (-1 if color is chess.BLACK else 1)
+
+    return total
+
+
+board = chess.Board()
+
+while not board.is_game_over():
+	print()
+	print(board)
+	print()
+	if board.turn:
+		while board.turn:
+			move = input('White Move:')
+			try:
+				board.push_san(move)
+			except:
+				print('Illegal move')
+
+	else:
+		move = findMove(3)
+		print('Black Move:', board.san(move))
+		board.push(move)
+
+print()
+print(board)
+print()
+print('Game is over')
